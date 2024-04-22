545a444b088289a35dc5bc9ec0d6423c813bacde
abs7798 <abs44@126.com>
2024-04-22 16:08:43 +0800

    sdsd

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import gradio as gr
 import os
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+from lmdeploy import pipeline, TurbomindEngineConfig
 
 # download internlm2 to the base_path directory using git tool
 base_path = './internlm2-chat-7b'
@@ -15,16 +14,15 @@ os.system(f'cd {base_path} && git lfs pull')
 os.system("pip install sentencepiece")
 os.system("pip install einops")
 
-tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+backend_config = TurbomindEngineConfig(session_len=8192) # 图片分辨率较高时请调高session_len
+# pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config) 非开发机运行此命令
+pipe = pipeline(base_path, backend_config=backend_config)
 
-def chat(message,history):
-    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
-        yield response
+def model(image, text):
+    response = pipe((text, image)).text
+    return [(text, response)]
 
-gr.ChatInterface(chat,
-                 title="InternLM2-Chat-7B",
-                description="""
-InternLM is mainly developed by Shanghai AI Laboratory.  
-                 """,
-                 ).queue(1).launch()
+demo = gr.Interface(fn=model, inputs=[gr.Textbox(),], outputs=gr.Chatbot())
+demo.launch()  
+
+
