602b5086bb1c3b26173661c6ca4c06bd3d2ffd81
abs7798 <129253305+abs7798@users.noreply.github.com>
2024-05-10 09:14:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ print(f'创建文件夹{os.path.isdir(base_path)}')
 #os.system("git lfs install")
 os.system(f'git clone https://code.openxlab.org.cn/abs7798/my_internlm_model.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
-#os.system("pip install sentencepiece")
-#os.system("pip install einops")
+os.system("pip install sentencepiece")
+os.system("pip install einops")
 
 '''from lmdeploy import pipeline, TurbomindEngineConfig
 backend_config = TurbomindEngineConfig(cache_max_entry_count=0.2) 
